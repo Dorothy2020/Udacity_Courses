631f8efc4a70aa58e61fb6353ec8af62735c653b
Dorothy_Akoth <dorothy@untapped-global.com>
2023-06-02 17:03:45 +0300

    python methods

---
+++ append_method.py
@@ -0,0 +1,36 @@
+# A helpful method called append adds an element to the end of a list.
+fruits = ["Oranges", "Avocado", "Quavas", "Kiwi", "Mango"]
+fruits.append("Banana")
+print(fruits)
+
+names = ["Carol","Albert","Ben","Donna"]
+print("&".join(sorted(names)))
+
+majina =["Carol","Albert","Ben","Donna"]
+majina.append("Eugenia")
+print(sorted(majina))
+
+# TUPLE
+tuple_a = 1,2
+tuple_b = (1,2)
+print(tuple_a == tuple_b)
+print(tuple_a[1])
+
+# SETS
+fruit = {"apple","banana","orange","grapefruit"}
+print("watermelon" in fruit)
+print(fruit)
+# Add an element
+fruit.add("watermelon")
+print(fruit)
+# remove a random element
+print(fruit.pop())
+
+x= [1,2,2,3,3,3,4,4,4,4]
+y= set(x)
+print(len(x) - len(y))
+
+a = [1,2,2,3,3,3,4,4,4,4]
+b = set(a)
+b.add(5)
+b.pop()
---
+++ join_method.py
@@ -0,0 +1,6 @@
+# A string list method  that takes in arguments consisting of a list elements joined by a separator string
+# use of "\n".join that does not print out the elements in a square bracket
+# "\n" as the separator so that there is a newline between each element
+new_str = "\n".join(["fore","aft","starboard","port"])
+print(new_str)
+
