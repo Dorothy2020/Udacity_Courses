e29f3091d3612d6ae6d2ec3e5a1f127349205c50
Dorothy_Akoth <dorothy@untapped-global.com>
2023-06-05 18:30:18 +0300

    worked on dictionaries

--- append_method.py
+++ append_method.py
@@ -3,21 +3,21 @@ fruits = ["Oranges", "Avocado", "Quavas", "Kiwi", "Mango"]
 fruits.append("Banana")
 print(fruits)
 
-names = ["Carol","Albert","Ben","Donna"]
+names = ["Carol", "Albert", "Ben", "Donna"]
 print("&".join(sorted(names)))
 
-majina =["Carol","Albert","Ben","Donna"]
+majina = ["Carol", "Albert", "Ben", "Donna"]
 majina.append("Eugenia")
 print(sorted(majina))
 
 # TUPLE
-tuple_a = 1,2
-tuple_b = (1,2)
+tuple_a = 1, 2
+tuple_b = (1, 2)
 print(tuple_a == tuple_b)
 print(tuple_a[1])
 
 # SETS
-fruit = {"apple","banana","orange","grapefruit"}
+fruit = {"apple", "banana", "orange", "grapefruit"}
 print("watermelon" in fruit)
 print(fruit)
 # Add an element
@@ -26,11 +26,11 @@ print(fruit)
 # remove a random element
 print(fruit.pop())
 
-x= [1,2,2,3,3,3,4,4,4,4]
-y= set(x)
+x = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+y = set(x)
 print(len(x) - len(y))
 
-a = [1,2,2,3,3,3,4,4,4,4]
+a = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
 b = set(a)
 b.add(5)
 b.pop()
---
+++ dictionaries.py
@@ -0,0 +1,6 @@
+capital_city = {"Nepal": "Kathmandu", "Italy": "Rome", "England": "London"}
+print(capital_city)
+
+# dictionary with keys and values of different data types
+numbers = {1: "One", 2: "Two", 3: "Three"}
+print(numbers)
